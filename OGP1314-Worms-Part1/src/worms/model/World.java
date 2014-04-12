0b4e445d21668e2d484c33db1b99f91bdd947840
pieter <pieterkusters1@msn.com>
2014-04-13 00:59:04 +0200

    class World basismethoden

--- OGP1314-Worms-Part1/src/worms/model/World.java
+++ OGP1314-Worms-Part1/src/worms/model/World.java
@@ -4,13 +4,47 @@ import java.util.Random;
 
 public class World {
 
-public World(double width, double height,boolean[][] passableMap, Random random) {	
+private double width, height;
+	
+public World(double width, double height) {	
+	if (!this.validSize(width) || !this.validSize(height))
+		throw new IllegalArgumentException(); // catch aan te vullen in facade
+	this.setWidth(width);
+	this.setHeight(height);
+}
+
+public void setWidth(double width) {
+	this.width = width;
+	}	
+
+public  double getWidth() {
+	return this.width;
+	}
+
+
+
+public void setHeight(double height) {
+	this.height = height;
+	}	
+
+public double getHeight() {
+	return this.height;
+	}
+
+
+public boolean validSize(double size) {
+	if (!Double.isNaN(size) &&  (0 <= size) && (size <= Double.MAX_VALUE))
+			{return true;}
+	else {return false;}
+	}
+	
+
+
+	
 	
 }
 
 
 
-	
-	
-	
-}
+
+
